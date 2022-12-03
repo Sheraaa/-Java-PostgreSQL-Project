cf75dd00c2c88187589d4260301cb4d6f00cc627
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-04 00:40:01 +0100

    added connection with database

--- src/test.java
+++ src/test.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class test {
 
@@ -13,58 +11,38 @@ public class test {
             System.exit(1);
         }
 
-
-        String url="jdbc:postgresql://localhost:5432/postgres";
-
-
+        String url = "jdbc:postgresql://localhost:5432/logiciel";
+        // String url="jdbc:postgresql://172.24.2.6:5432/dbchehrazadouazzani“  <-- A MODIFIER
         Connection conn = null;
 
-
         try {
-            conn = DriverManager.getConnection(url,"postgres","Mariam-16");
+            //conn=DriverManager.getConnection(url,”dbchehrazadouazzani”,”SQINPAG0B”);
+            conn = DriverManager.getConnection(url, "postgres", "shera");
         } catch (SQLException e) {
             System.out.println("Impossible de joindre le server !");
             System.exit(1);
         }
 
-        /*
-        Statement = instruction(s) à envoyer à la BD
-
-                try {
-                    Statement s = conn.createStatement();
-                    s.executeUpdate("INSERT INTO exercice.utilisateurs "+
-                            "VALUES (DEFAULT, ‘Damas', 'Christophe');");
-                } catch (SQLException se) {
-                    System.out.println("Erreur lors de l’insertion !");
-                    se.printStackTrace();
-                    System.exit(1);
-                }
-
-
-        ResultSet = Résultat d’un SELECT
-
-                try {
-                    Statement s = conn.createStatement();
-                    try(ResultSet rs= s.executeQuery("SELECT nom"+                              ----> Ceci n'est pas un try...catch...finally !
-                                                     "FROM exercice.utilisateurs;"){                  C'est un try avec ressource
-                        while(rs.next()) {                                                            => la ressource sera fermée à la fin du bloc
-                            System.out.println(rs.getString(1));                                      => équivalent à un finally {rs.close();}
-                        }
-                    }
-                } catch (SQLException se) {
-                    se.printStackTrace();
-                    System.exit(1);
-                }
-
-
-
-
-
-
-
-
-        */
+        try {
+            Statement s = conn.createStatement();
+            s.executeUpdate("INSERT INTO logiciel.etudiants(nom,prenom,mail) " +
+                    "VALUES ('CO', 'co','co12@student.vinci.be');");
+        } catch (SQLException se) {
+            System.out.println("Erreur lors de l’insertion !");
+            se.printStackTrace();
+            System.exit(1);
+        }
+//        try {
+//            Statement s = conn.createStatement();
+//            try (ResultSet rs = s.executeQuery("SELECT nom" +
+//                    "FROM exercice.utilisateurs;")){
+//                while (rs.next()) {
+//                    System.out.println(rs.getString(1));
+//                }
+//            }
+//        } catch (SQLException se) {
+//            se.printStackTrace();
+//            System.exit(1);
+//        }
     }
-
-
 }
